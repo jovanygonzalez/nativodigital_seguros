86c81a23af7a12cb22b8af40ee6b7f431fd9995a
jovanygonzalez <jovany0.0gonzalez@gmail.com>
2021-06-20 16:19:06 -0500

    correción de llaves

---
+++ dist/keys.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    database: {
+        host: "localhost",
+        user: "id17097185_userseguros",
+        password: "#PASSseguros#1",
+        database: "id17097185_dbseguros"
+    }
+};
+// host: "localhost",
+// user: "USERseguros",
+// password: "#PASSseguros#1",
+// database: "DBseguros"
+//# sourceMappingURL=keys.js.map
